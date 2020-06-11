f6b02a6f8568855f1df4687b98c86570a4643256
ogent <38910643+o-gent@users.noreply.github.com>
2020-06-11 16:09:32 +0100

    setup heroku tokens

--- fastmental/config.py
+++ fastmental/config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
     """
     config variables for app
     """
     SECRET_KEY = "supersekrit"
     WIT_TOKEN = "?"
-    FB_PAGE_TOKEN = "?"
-    FB_VERIFY_TOKEN = "fb"
+    FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN")
+    FB_VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
