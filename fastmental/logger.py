679b6aadae3b7169d7673b1cc4a812e05cb58d85
ogent <38910643+o-gent@users.noreply.github.com>
2020-06-12 12:42:37 +0100

    fix logger

--- fastmental/logger.py
+++ fastmental/logger.py
@@ -7,13 +7,11 @@ def setup_logger(name, log_file, level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     stream = logging.StreamHandler()
     
-    handler.setFormatter(formatter)
     stream.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    logger.addHandler(handler)
     logger.addHandler(stream)
 
     return logger
