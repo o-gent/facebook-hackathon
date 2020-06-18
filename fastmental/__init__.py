e8457361c512130130fb57b0f7554308f20cbb25
ogent <olivergent@btinternet.com>
2020-06-18 22:48:36 +0100

    fixes

--- fastmental/__init__.py
+++ fastmental/__init__.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI
-from fastmental.routers import main, messenger
-
-
 def create_app():
     """
     Create the app instance
     """
+    from fastapi import FastAPI
+    from fastmental.routers import main, messenger
     app = FastAPI()
     app.include_router(main.router)
     app.include_router(messenger.router)
