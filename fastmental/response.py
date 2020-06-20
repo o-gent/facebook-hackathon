4115c53bbc704b66f9611cc3e3099fa613bc0224
goconnell102 <61713548+goconnell102@users.noreply.github.com>
2020-06-20 16:18:32 +0100

    Update response.py

--- fastmental/response.py
+++ fastmental/response.py
@@ -8,34 +8,45 @@ from fastmental.logger import setup_logger
 logger = setup_logger("messenger_router", "logs/messenger_router.log")
 
 
-def fb_message(sender_id: int, text: str, quick_replies: List[str] = []):
+def fb_message(sender_id: int, text: str or List[str], quick_replies: List[str] = []):
     """
     Function for returning response to messenger
-    :quick_replies: an optional keyword arguement
+    :quick_replies: an optional keyword arguement. Only sends after last argument
     """
 
-    data: dict = {
-        'recipient': {'id': sender_id},
-        'message': {
-            'text': text
-            }
-    }
+    if type(text) is str:
+	text=[text]
 
-    # convert the quick reply options into the correct format and add to the payload
-    if quick_replies != []:
-        # payload is set to the same as the title, but these could be different
-        quick_reply = [{'content_type': 'text', 'title': reply, 'payload': reply} for reply in quick_replies]
-        data['message']['quick_replies'] = quick_reply
+    idx=0
+    while idx<len(text):
 
-    # Setup the query string with your PAGE TOKEN
-    qs = 'access_token=' + Config.FB_PAGE_TOKEN
+	msgText=text[idx]        
+
+	data: dict = {
+            'recipient': {'id': sender_id},
+            'message': {
+                'text': msgText
+                }
+        }
+
+        # convert the quick reply options into the correct format and add to the payload
+        if quick_replies != []:
+            # payload is set to the same as the title, but these could be different
+            quick_reply = [{'content_type': 'text', 'title': reply, 'payload': reply} for reply in quick_replies]
+            data['message']['quick_replies'] = quick_reply
+
+        # Setup the query string with your PAGE TOKEN
+        qs = 'access_token=' + Config.FB_PAGE_TOKEN
     
-    logger.info(f"sending {text} with quick replies: {quick_replies} to {sender_id}")
+        logger.info(f"sending {msgText} with quick replies: {quick_replies} to {sender_id}")
     
-    # Send POST request to messenger
-    resp = requests.post(
-        'https://graph.facebook.com/me/messages?' + qs,
-        json=data
-    )
+        # Send POST request to messenger
+        resp = requests.post(
+            'https://graph.facebook.com/me/messages?' + qs,
+            json=data
+        )
 
-    logger.info(f"got message send response {str(resp.content)}")
+	if resp['message_id'] !="":
+	    idx+=1
+
+        logger.info(f"got message send response {str(resp.content)}")
